a0c4c5da09924b480f3a84ee95b211c1cd161b97
westonz7042 <quadruple.ww@gmail.com>
2025-01-13 22:38:52 -0800

    deleted unnecessary errors/contact.ts

--- backend/src/errors/contact.ts
+++
@@ -1,9 +0,0 @@
-import { CustomError } from "./errors";
-const INVALID_FORM = "Name, email, and phone are required fields.";
-const INVALID_PHONE = "Invalid phone number format.";
-const INVALID_EMAIL = "Invalid email format.";
-export class ContactError extends CustomError {
-  static INVALID_FORM = new ContactError(0, 400, INVALID_FORM);
-  static INVALID_PHONE = new ContactError(1, 400, INVALID_PHONE);
-  static INVALID_EMAIL = new ContactError(2, 400, INVALID_EMAIL);
-}
