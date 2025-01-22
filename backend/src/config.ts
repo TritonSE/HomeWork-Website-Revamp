7c0e3c7ceac2c15a9852c5bf18242d7d46cf91de
PoliteUnicorn <yankaren15@gmail.com>
2025-01-22 01:16:15 -0800

    untested routes

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -8,4 +8,7 @@ dotenv.config();
 if (!process.env.PORT) throw InternalError.NO_APP_PORT;
 const port = process.env.PORT;
 
-export { port };
+if (!process.env.MONGO_URI) throw InternalError.NO_MONGO_URI;
+const mongoUri = process.env.MONGO_URI;
+
+export { port, mongoUri };
