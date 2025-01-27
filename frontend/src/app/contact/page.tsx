41acd0aece4ce08d73748ba4651d12578b81dc03
Weston Zong <62188155+westonz7042@users.noreply.github.com>
2025-01-26 19:32:34 -0800

    Weston/Frontend Contact Request Form (#35)
    * basic form
    
    * added contact form
    
    * frontend lint fixes

---
+++ frontend/src/app/contact/page.tsx
@@ -0,0 +1,7 @@
+import ContactForm from "../../components/contactForm";
+
+const ContactPage = () => {
+  return <ContactForm />;
+};
+
+export default ContactPage;
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,12 +1,20 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: ["./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
+  content: [
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/api/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+      },
+      fontFamily: {
+        golos: ['"GolosText"', "sans-serif"],
+        manrope: ['"Manrope"', "sans-serif"],
       },
     },
   },
