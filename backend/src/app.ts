182741446de1d3e6443c08ec669abc5fc4a11098
exe1x <exefnbr@gmail.com>
2025-01-27 13:50:57 -0800

    Merge with main

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,11 +7,13 @@ import { mongoUri, port } from "./config";
 import { errorHandler } from "./errors/handler";
 import articleRoutes from "./routes/article";
 import contactRoute from "./routes/contactRequest";
+import userRoute from "./routes/user";
 // Initialize Express App
 const app = express();
 app.use(cors({ origin: process.env.FRONTEND_ORIGIN }));
 app.use(json());
 app.use("/api", contactRoute);
+app.use("/api/users", userRoute);
 app.use("/api/articles", articleRoutes);
 app.use(errorHandler);
 mongoose
