8ce188f662804087c36c884a99b1e5dff98179bb
Edward Lin <63514282+EdwardLinS@users.noreply.github.com>
2025-01-29 14:39:22 -0800

    Create news and events carousel component (#27)
    * Add tailwind setup
    
    * Add custom GolosText and Manrope fonts to config
    
    * Create carousel card component
    
    * Create carousel container
    
    * Add mobile responsiveness
    
    * Change img to Image to optimize loading
    
    * Clean code format with formatter
    
    * Add comments to improve component readability
    
    * Fix carousel button sizing on mobile
    
    * Fix carousel button wrong position issue
    
    * Remove scrollbar from carousel
    
    * Make mobile size carousel arrows larger
    
    * Change viewbox size of card image to static
    
    * Fix format of global css
    
    * Change arrow size to match design
    
    * Re-add tailwind back to pages
    
    * Update style to match new designs
    
    * Fix code style with prettier
    
    * Merge branch 'main' into EdwardLinS/component-news-and-events-carousel

---
+++ frontend/src/components/EventsCarousel/EventsCarouselCard.tsx
@@ -0,0 +1,52 @@
+import { StaticImport } from "next/dist/shared/lib/get-img-props";
+import Image from "next/image";
+
+import learnMoreIcon from "@/../public/icons/learn-more.svg";
+
+type Event = {
+  header: string;
+  dateCreated: string;
+  body: string;
+  thumbnail: string;
+  thumbnailAlt?: string;
+  learnMoreUrl: string;
+};
+
+type EventProps = {
+  event: Event;
+};
+
+/**
+ * Card to display event information in the EventsCarousel component
+ */
+export const EventsCarouselCard: React.FC<EventProps> = ({ event }) => {
+  return (
+    <div
+      className="
+            flex-shrink-0 sm:w-2/5 w-full max-w-xl h-full p-4 pt-5 pb-5 
+            rounded-lg border border-gray-400 shadow-md 
+            snap-normal snap-start"
+    >
+      <div className="flex flex-col gap-2 w-full">
+        <div className="relative w-full h-72">
+          <Image
+            src={event.thumbnail}
+            alt={event.thumbnailAlt ?? event.header}
+            layout="fill"
+            objectFit="cover"
+          />
+        </div>
+        <h3 className="font-golos md:text-lg text-sm font-medium">{event.header}</h3>
+        <p className="font-manrope md:text-sm text-xs text-orange-500 font-bold">
+          {event.dateCreated}
+        </p>
+        <p className="font-golos md:text-sm text-xs font-light line-clamp-3">{event.body}</p>
+      </div>
+
+      <a href={event.learnMoreUrl} className="flex flex-row justify-end gap-2 w-full mt-6">
+        <span className="font-manrope md:text-sm text-xs font-bold text-gray-400">LEARN MORE</span>
+        <Image src={learnMoreIcon as StaticImport} alt="Learn more arrow" />
+      </a>
+    </div>
+  );
+};
