66532f8458e65c7f6d9ebb4eeb047edce65da6f4
EdwardLinS <edwardlin4573@gmail.com>
2025-01-29 17:27:12 -0800

    Add footer within theme provider

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -20,8 +20,8 @@ export default function RootLayout({
           }}
         >
           {children}
+          <Footer />
         </ThemeProvider>
-        <Footer />
       </body>
     </html>
   );
