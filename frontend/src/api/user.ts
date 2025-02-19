b09e6d2e460fa5556df108ca8d682aece67089ba
kilehsu <kilehsu@gmail.com>
2025-02-18 17:31:11 -0800

    Added user signup page

--- frontend/src/api/user.ts
+++ frontend/src/api/user.ts
@@ -1,9 +1,15 @@
-import { APIResult, get } from "./requests";
+import { APIResult, get, post } from "./requests";
 
 export type User = {
   id: string;
   email: string;
 };
+
+export type UserCreate = {
+  _id: string;
+  name: string;
+  email: string;
+}
 
 export async function verifyUser(token: string): Promise<APIResult<User>> {
   try {
@@ -11,6 +17,20 @@ export async function verifyUser(token: string): Promise<APIResult<User>> {
       Authorization: `Bearer ${token}`,
     });
     const data = (await response.json()) as User;
+    return { success: true, data };
+  } catch (error) {
+    return { success: false, error: String(error) };
+  }
+}
+
+export async function createUser(name: string, email: string, password: string): Promise<APIResult<UserCreate>> {
+  try {
+    const response = await post("/users/create", {
+      name,
+      email,
+      password
+    });
+    const data = (await response.json()) as UserCreate;
     return { success: true, data };
   } catch (error) {
     return { success: false, error: String(error) };
