08758bf613f8706e55513bc5d7145e1011052952
PoliteUnicorn <yankaren15@gmail.com>
2025-02-26 22:13:54 -0800

    Clean up css and convert to tailwind

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-
-import styles from "./Header.module.css"; // Import CSS Module for styling
 import "@fontsource/golos-text"; // Defaults to weight 400
 
 export type HeaderComponents = {
@@ -12,16 +10,21 @@ export type HeaderComponents = {
 const Header = ({ imageUrl, header, subheader }: HeaderComponents) => {
   return (
     <div
-      className={styles.headerContainer}
+      className="flex flex-col justify-center items-start gap-[179px] p-[20px] opacity-0"
       style={{
+        height: "940px",
         backgroundImage: `linear-gradient(180deg, rgba(0, 0, 0, 0.36) 0%, rgba(0, 0, 0, 0.6) 100%), url(${imageUrl})`,
         backgroundSize: "cover", // Ensure the background image covers the entire area
         backgroundPosition: "center", // Center the image
       }}
     >
-      <div className={styles.textContainer}>
-        <div className={styles.header}>{header}</div>
-        <div className={styles.subheader}>{subheader}</div>
+      <div className="flex flex-col items-start gap-[10px] max-w-[1336px] opacity-0 mx-[52px]">
+        <div className="text-white text-left font-[Libre Baskerville] text-[64px] font-bold leading-[96px] max-w-[1195px]">
+          {header}
+        </div>
+        <div className="text-white text-left font-[Golos Text] text-[20px] font-normal leading-[26px] max-w-[790px] opacity-0 mt-0">
+          {subheader}
+        </div>
       </div>
     </div>
   );
