29323cabfd860d1df370ce0323d689f2f85a29a3
EdwardLinS <edwardlin4573@gmail.com>
2025-02-26 18:23:43 -0800

    Fix style issues 2

--- frontend/src/app/events-archive/page.tsx
+++ frontend/src/app/events-archive/page.tsx
@@ -110,9 +110,9 @@ const EventsArchive = () => {
         <section className="flex flex-col gap-5 mb-5 font-golos">
           <h1 className="text-2xl sm:text-4xl font-medium">Events Archive</h1>
           <p className="text-sm sm:text-base">
-            Our Past Events archive showcases a rich history of engagement and learning opportunities.
-            From insightful workshops to vibrant community gatherings, explore the impactful
-            activities that have brought people together.
+            Our Past Events archive showcases a rich history of engagement and learning
+            opportunities. From insightful workshops to vibrant community gatherings, explore the
+            impactful activities that have brought people together.
           </p>
         </section>
         <section className="flex flex-col gap-5 mb-5">
