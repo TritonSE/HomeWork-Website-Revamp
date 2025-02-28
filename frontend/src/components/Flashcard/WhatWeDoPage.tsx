777f95080411a97f8cde80658a5bca973103a977
westonz7042 <quadruple.ww@gmail.com>
2025-02-27 23:37:51 -0800

    added extra component to whatwedo

--- frontend/src/components/Flashcard/WhatWeDoPage.tsx
+++ frontend/src/components/Flashcard/WhatWeDoPage.tsx
@@ -1,14 +1,6 @@
 "use client";
 import React from "react";
-
 import Flashcard from "./Flashcard";
-
-// import Flashcard1 from "@/../public/images/flashcards/flashcard1.png";
-// import Flashcard2 from "@/../public/images/flashcards/flashcard2.png";
-// import Flashcard3 from "@/../public/images/flashcards/flashcard3.png";
-// import Flashcard4 from "@/../public/images/flashcards/flashcard4.png";
-// import Flashcard5 from "@/../public/images/flashcards/flashcard5.png";
-// import Flashcard6 from "@/../public/images/flashcards/flashcard6.png";
 
 const FlashcardPage: React.FC = () => {
   const flashcards = [
@@ -45,24 +37,53 @@ const FlashcardPage: React.FC = () => {
   ];
 
   return (
-    <div style={{ padding: "50px" }}>
-      <div className="text-5xl font-golos pb-8">Our Model</div>
-      <div className="flex flex-col">
-        {flashcards.map((flashcard, index) => (
-          <div key={index} className="relative flex flex-col">
-            <div
-              className={`flex ${index % 2 === 0 ? "flex-row" : "flex-row-reverse"} w-full items-center`}
-            >
-              <Flashcard {...flashcard} />
-              {index < 5 && (
-                <>
-                  <div className="w-[calc(100%-724.5px)] h-[5px] bg-gray-600" />
-                  <div className="h-[206.5px] w-[5px] bg-gray-600 mt-auto" />
-                </>
-              )}
+    <div>
+      <div className="flex md:flex-row flex-col w-full justify-between pt-12">
+        <div className="font-golos  md:w-[calc(40%)] w-full px-8 md:pb-0 pb-20">
+          <div className="text-[32px] mb-8 weight-500 ">
+            Transforming Lives, Empowering Communities
+          </div>
+          <div className="text-[20px] weight-400">
+            <div>
+              By leveraging community resources, we establish a supportive peer mentoring
+              environment that empowers San Diegans towards lasting change and success.
+            </div>
+            <div className="mt-6">
+              Our transformative approach to post-incarceration support emphasizes continuous
+              assistance during employment rather than solely focusing on pre-employment readiness.
+              Through collaborative efforts, we are committed to facilitating a seamless transition
+              for individuals returning from incarceration. Our Friday night classes serve as a
+              gateway to Union careers. To enhance engagement, we organize unique in-person
+              gatherings six times a year, bringing together Trade Union representatives and
+              individuals with diverse incarceration experiences. These gatherings foster
+              discussions on high-wage careers, apprenticeship programs, and occupational skills
+              training.
             </div>
           </div>
-        ))}
+        </div>
+        <div className="md:px-8">
+          <img src="/images/whatwedo.png" className="h-[calc(100%)] object-contain w-full" />
+        </div>
+      </div>
+      <div className="px-8">
+        <div className="text-5xl font-golos pb-8 mt-20">Our Model</div>
+        <div className="flex flex-col">
+          {flashcards.map((flashcard, index) => (
+            <div key={index} className="relative flex flex-col">
+              <div
+                className={`flex ${index % 2 === 0 ? "flex-row" : "flex-row-reverse"} w-full items-center`}
+              >
+                <Flashcard {...flashcard} />
+                {index < 5 && (
+                  <>
+                    <div className="w-[calc(100%-724.5px)] h-[5px] bg-gray-600" />
+                    <div className="h-[206.5px] w-[5px] bg-gray-600 mt-auto" />
+                  </>
+                )}
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
