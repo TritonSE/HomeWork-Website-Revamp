0cdb5041f5f6a2b6dde2e2a9f50743e374578fde
yunshan <156245615+yyunshanli@users.noreply.github.com>
2025-04-10 13:32:24 -0700

    updated boxlink picture according to figma

--- frontend/src/components/BoxLinkGroup.tsx
+++ frontend/src/components/BoxLinkGroup.tsx
@@ -7,38 +7,30 @@ const BoxLinkGroup = () => {
     <div className="w-full flex-col md:grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] flex-wrap items-end">
       <BoxLink
         tall={false}
-        src={
-          "https://plus.unsplash.com/premium_photo-1661497675847-2075003562fd?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y29ycG9yYXRlfGVufDB8fDB8fHww"
-        }
+        src={"/images/boxlink1.png"}
         header={"What We Do"}
-        body={"Lorem ipsum t labore et dolore magna aliqua."}
+        body={"Find out how we support our members and our community."}
         link={"/what-we-do"}
       />
       <BoxLink
         tall={true}
-        src={
-          "https://plus.unsplash.com/premium_photo-1661497675847-2075003562fd?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y29ycG9yYXRlfGVufDB8fDB8fHww"
-        }
-        header={"What We Do"}
-        body={"Lorem ipsum dol aliqua."}
+        src={"/images/boxlink2.png"}
+        header={"About Us"}
+        body={"Learn about our history, mission, vision and our incredible team."}
         link={"/what-we-do"}
       />
       <BoxLink
         tall={false}
-        src={
-          "https://plus.unsplash.com/premium_photo-1661497675847-2075003562fd?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y29ycG9yYXRlfGVufDB8fDB8fHww"
-        }
-        header={"What We Do"}
-        body={"Lorem ipsum dolor sit amet ut labore et dolore magna aliqua."}
+        src={"/images/boxlink3.png"}
+        header={"Get Involved"}
+        body={"Join us on our mission and check out upcoming events."}
         link={"/what-we-do"}
       />
       <BoxLink
         tall={true}
-        src={
-          "https://plus.unsplash.com/premium_photo-1661497675847-2075003562fd?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y29ycG9yYXRlfGVufDB8fDB8fHww"
-        }
-        header={"What We Do"}
-        body={"Lorem ipsum dolor sit amet, consectetur."}
+        src={"/images/boxlink4.png"}
+        header={"Stay Connected"}
+        body={"Stay informed about our past events and get in touch with us."}
         link={"/what-we-do"}
       />
     </div>
