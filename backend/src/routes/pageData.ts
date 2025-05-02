4f3ef0dc867ebc506e26320a636971313579f26f
NumaanFormoli <numanformoliz@gmail.com>
2025-05-02 09:30:31 -0700

    commented create/delete routes

--- backend/src/routes/pageData.ts
+++ backend/src/routes/pageData.ts
@@ -15,7 +15,7 @@ router.post("/update", [verifyAuthToken], updatePageDataValidator, updatePageDat
 router.get("/all", getAllPageData);
 
 // Added in case you need to make delete or make a page.
-router.delete("/delete/:pagename", [verifyAuthToken], deletePageData);
-router.post("/seed", [verifyAuthToken], seedPageData);
+// router.delete("/delete/:pagename", [verifyAuthToken], deletePageData);
+// router.post("/seed", [verifyAuthToken], seedPageData);
 
 export default router;
