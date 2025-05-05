e964622da31629d640f05e87453bda44ef49dde4
westonz7042 <quadruple.ww@gmail.com>
2025-05-04 18:54:57 -0700

    lint fix

--- frontend/src/app/(admin)/test-page/page.tsx
+++ frontend/src/app/(admin)/test-page/page.tsx
@@ -1,6 +1,12 @@
-import TestPageComponent from "../../../components/testPageComponent";
+"use client";
+import TestPage from "../../../components/testPageComponent";
+import AdminNavBar from "../../../components/NavBar/AdminNavBar";
 const testPage = () => {
-  return <TestPageComponent />;
+  return (
+    <>
+      <TestPage /> <AdminNavBar />
+    </>
+  );
 };
 
 export default testPage;
---
+++ frontend/src/components/NavBar/AdminNavBar.tsx
@@ -0,0 +1,110 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { useAuthState } from "../../contexts/userContext";
+const Navbar: React.FC<{ opaque?: boolean }> = ({ opaque = false }) => {
+  const router = useRouter();
+  const { loading, firebaseUser, logout } = useAuthState();
+  const displayEmail = firebaseUser?.email;
+  const displayName = firebaseUser?.displayName;
+  const [open, setOpen] = useState(false);
+  const toggle = () => {
+    setOpen((o) => !o);
+  };
+  const handleSignOut = async () => {
+    await logout();
+    router.replace("/login");
+  };
+  return (
+    <>
+      {opaque && <div className="w-[176px] h-[98px]" />}
+      <nav className="fixed top-0 left-0 w-auto z-50 flex flex-col items-center px-8 py-4 transition-all duration-300">
+        <div className="flex items-center">
+          <Link href="/">
+            <Image src="/logo-dark.png" alt="Logo" width={176} height={98} priority />
+          </Link>
+        </div>
+
+        <div className="flex flex-col text-[20px] mt-[25vh] font-GolosText text-[#1B1B1B] font-normal">
+          {[
+            {
+              title: "Page Editor",
+              href: "/page-editor",
+              icon: "/images/adminNavBar/pageEditorIcon.png",
+            },
+            {
+              title: "Event Manager",
+              href: "/event-manager",
+              icon: "/images/adminNavBar/eventManagerIcon.png",
+            },
+            {
+              title: "Mailing List",
+              href: "/mailing-list",
+              icon: "/images/adminNavBar/mailingListIcon.png",
+            },
+          ].map((item) => (
+            <Link
+              key={item.title}
+              href={item.href}
+              className="w-full px-6 py-3 mt-4 flex items-center hover:bg-[#D9D9D9] transition-colors duration-150 rounded-xl"
+            >
+              <Image
+                src={item.icon}
+                alt={`${item.title} icon`}
+                width={16}
+                height={16}
+                className="mr-4"
+              />
+              {item.title}
+            </Link>
+          ))}
+        </div>
+        <div className="fixed mb-8 bottom-0">
+          <div className="flex items-center space-x-2 cursor-pointer" onClick={toggle}>
+            <div className="flex flex-col">
+              <span className="text-[20px] font-medium">
+                {loading ? "Loading..." : firebaseUser ? displayName : "Not signed in"}
+              </span>
+              <span className="text-[16px] font-normal">{displayEmail}</span>
+            </div>
+            <Image
+              src={open ? "/images/adminNavBar/upArrow.png" : "/images/adminNavBar/downArrow.png"}
+              alt={open ? "upArrow" : "downArrow"}
+              width={16}
+              height={16}
+              className="object-contain flex-shrink-0"
+            />
+          </div>
+
+          {/* dropdown menu */}
+          {open && (
+            <div className="absolute bottom-full mb-2 left-0 w-48 bg-white border shadow-lg overflow-hidden">
+              <Link
+                href="/create-account"
+                className="block px-4 py-2 hover:bg-gray-100 text-center"
+              >
+                Create an Account
+              </Link>
+              <Link
+                href="/change-password"
+                className="block px-4 py-2 hover:bg-gray-100 text-center"
+              >
+                Change Password
+              </Link>
+              <button
+                onClick={() => void handleSignOut}
+                className="w-full px-4 py-2 hover:bg-gray-100 text-center"
+              >
+                Sign Out
+              </button>
+            </div>
+          )}
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
