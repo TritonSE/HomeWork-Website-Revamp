3536c8dbc57be69a72f541ed2fb559173e858d0c
Michael Sullivan <96799955+mraysu@users.noreply.github.com>
2025-09-06 21:20:22 -0700

    implement bulk-delete (#136)

--- frontend/src/components/MailingList.tsx
+++ frontend/src/components/MailingList.tsx
@@ -336,12 +336,34 @@ const MailingList: React.FC = () => {
           <Table
             columns={columns}
             data={filtered}
-            className="w-full tse-table group/row"
+            className="w-full tse-table group/row z-10"
             enablePagination={true}
             enableSorting={true}
             enableGlobalFiltering={false}
           />
         )}
+      </div>
+      <div className={`flex justify-end ${selectionCount === 0 ? "hidden" : ""}`}>
+        <button
+          onClick={() => {
+            setShowConfirm(true);
+          }}
+          className="rounded-md max-h-12 border-[2px] -mt-16 border-[#B93B3B] text-xl text-[#B93B3B] px-6 py-2 flex items-center gap-2 hover:bg-gray-100"
+        >
+          {`Delete Contacts (${selectionCount})`}
+          <svg
+            width="14"
+            height="14"
+            viewBox="0 0 14 14"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M1.70711 0.292894C1.31658 -0.0976312 0.683417 -0.0976312 0.292893 0.292894C-0.0976311 0.683418 -0.0976311 1.31658 0.292893 1.70711L5.58579 7L0.292893 12.2929C-0.0976311 12.6834 -0.0976311 13.3166 0.292893 13.7071C0.683417 14.0976 1.31658 14.0976 1.70711 13.7071L7 8.41421L12.2929 13.7071C12.6834 14.0976 13.3166 14.0976 13.7071 13.7071C14.0976 13.3166 14.0976 12.6834 13.7071 12.2929L8.41421 7L13.7071 1.70711C14.0976 1.31658 14.0976 0.683419 13.7071 0.292895C13.3166 -0.0976297 12.6834 -0.0976297 12.2929 0.292895L7 5.58579L1.70711 0.292894Z"
+              fill="#B93B3B"
+            />
+          </svg>
+        </button>
       </div>
       {/* styles (unchanged + new checkbox rules) */}
       <style jsx>{`
