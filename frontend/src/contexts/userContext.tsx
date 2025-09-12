438ab0301cb6c22bc4bed66db3dc07ff476ab367
mraysu <mike.ray.sullivan@gmail.com>
2025-09-12 10:58:05 -0700

    Frontend Privileged View

--- frontend/src/contexts/userContext.tsx
+++ frontend/src/contexts/userContext.tsx
@@ -3,34 +3,51 @@
 import { User as FirebaseUser, onAuthStateChanged, signOut } from "firebase/auth";
 import { ReactNode, createContext, useContext, useEffect, useState } from "react";
 
+import { verifyUser } from "@/api/user";
 import { initFirebase } from "@/firebase/firebase";
 
 type AuthState = {
   loading: boolean;
   firebaseUser: FirebaseUser | null;
   logout: () => Promise<void>;
+  privileged: boolean;
 };
 const UserContext = createContext<AuthState>({
   loading: true,
   firebaseUser: null,
   logout: () => Promise.resolve(),
+  privileged: false,
 });
 
 export const UserContextProvider = ({ children }: { children: ReactNode }) => {
   const { auth } = initFirebase();
-  const [state, setState] = useState<Omit<AuthState, "logout">>({
+  const [state, setState] = useState<{ loading: boolean; firebaseUser: FirebaseUser | null }>({
     loading: true,
     firebaseUser: null,
   });
+  const [privileged, setPrivileged] = useState<boolean>(false);
   const logout = () => signOut(auth);
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (fbUser) => {
       setState({ loading: false, firebaseUser: fbUser });
+      void updatePrivilege(fbUser);
     });
     return unsubscribe;
   }, [auth]);
 
-  return <UserContext.Provider value={{ ...state, logout }}>{children}</UserContext.Provider>;
+  const updatePrivilege = async (fbUser: FirebaseUser | null) => {
+    if (fbUser !== null) {
+      const token = await fbUser.getIdToken();
+      const result = await verifyUser(token);
+      if (result.success) {
+        setPrivileged(result.data.privileged ?? false);
+      }
+    }
+  };
+
+  return (
+    <UserContext.Provider value={{ ...state, logout, privileged }}>{children}</UserContext.Provider>
+  );
 };
 
 export const useAuthState = () => useContext(UserContext);
